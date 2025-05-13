f5034f22f07181482e4ebc61b86dfe3cfa5669c8
ethanhkatz <ethanhkatz@gmail.com>
2025-05-13 14:22:02 -0600

    Made thing to determine lambda layers.

--- utilities/tester.py
+++ utilities/tester.py
@@ -1,3 +1,12 @@
 import sys
+import json
 
-print(sys.argv)
+changed_directories = set(sys.argv[1].split(' '))
+
+with open("lambda_layers_map.json", 'r') as infile:
+    lambda_layers_map = json.load(infile)
+
+with open("layers_to_build.txt", 'w') as outfile:
+    for layer, dirs in lambda_layers_map.items():
+        if any(dir in changed_directories for dir in dirs):
+            outfile.write(f"{layer} {' '.join(dirs)}\n") # write layer and dirs in a space-separated line
